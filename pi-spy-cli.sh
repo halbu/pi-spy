7e50eff54f4ea33f09ad8cdc3d8c89d2ac63cfe1
halbu <fstream.c@gmail.com>
2019-05-10 20:30:19 +0100

    Nicer tabular output with column headers &c

--- pi-spy-cli.sh
+++ pi-spy-cli.sh
@@ -1,10 +1,17 @@
+# Path, colors, substrings
 path=~/pi-spy/pi-spy.db
-deg=$'\xc2\xb0'
-
-# Get ROWID of the most recent reading in the db
-maxrow=$(sqlite3 $path "select max(rowid) from readings;")
+hi=$'\e[1;91m'
+lo=$'\e[1;32m'
+cu=$'\e[1;93m'
+nu=$'\e[1;96m'
+grey=$'\e[1;90m'
+lgrey=$'\e[1;37m'
+def=$'\e[0m'
+br=$grey"||"$def
+lbr=$grey"|"$def
 
 # Get current (i.e. most recent) readings
+maxrow=$(sqlite3 $path "select max(rowid) from readings;")
 cur=$(sqlite3 $path "select temp, cpu_util, mem_used from readings where rowid=\"$maxrow\";")
 cur=(${cur//|/ })
 cur_t=$(echo ${cur[0]}°C)
@@ -31,29 +38,22 @@ max_t_1h=$(echo ${data_1h[0]}°C)
 max_c_1h=$(echo ${data_1h[1]}%)
 max_t_10m=$(echo ${data_10m[0]}°C)
 max_c_10m=$(echo ${data_10m[1]}%)
-
-# Total memory
-tot_m=$(echo ${data_1h[2]}M)
 max_m_1d=$(echo ${data_1d[5]}M)
 max_m_1h=$(echo ${data_1h[3]}M)
 max_m_10m=$(echo ${data_10m[3]}M)
 min_m_1d=$(echo ${data_1d[7]}M)
+tot_m=$(echo ${data_1h[2]}M)
 
-hi=$'\e[1;91m'
-lo=$'\e[1;32m'
-cu=$'\e[1;93m'
-nu=$'\e[1;96m'
-grey=$'\e[1;90m'
-lgrey=$'\e[1;37m'
-def=$'\e[0m'
-br=$grey"||"$def
-lbr=$grey"|"$def
+printf "\n"$grey"%-15s"$def" "$br"  %-14s "$br"  %-12s "$br"  %-14s "$lbr"  %-14s "$lbr"  %-14s\n" \
+"pi-spy" "Most Recent" "Min (today)" "Max (last 10m)" "Max (last 1hr)" "Max (today)"
 
-printf "Current Temp: "$cu"%-8s"$def"       "$br"  24hr low: "$lo"%-8s"$def"  "$br"  Maxes: "$hi"%-7s"$def" (10m)  "$lbr"  "$hi"%-7s"$def" (1h)  "$lbr"  "$hi"%-7s"$def" (1d)\n" \
-$cur_t $min_t_1d $max_t_10m $max_t_1h $max_t_1d
+printf $grey"----------------┤├-----------------┤├---------------┤├-----------------┼-----------------┼---------------\n"$def
 
-printf "Current CPU:  "$cu"%-7s"$def"       "$br"  24hr low: "$lo"%-7s"$def"  "$br"  Maxes: "$hi"%-6s"$def" (10m)  "$lbr"  "$hi"%-6s"$def" (1h)  "$lbr"  "$hi"%-6s"$def" (1d)\n" \
-$cur_c $min_c_1d $max_c_10m $max_c_1h $max_c_1d
+printf "%-15s "$br"  "$cu"%-15s"$def" "$br"  "$lo"%-13s"$def" "$br"  "$hi"%-15s"$def" "$lbr"  "$hi"%-15s"$def" "$lbr"  "$hi"%-15s"$def"\n" \
+"Temperature" $cur_t $min_t_1d $max_t_10m $max_t_1h $max_t_1d
 
-printf "Current Mem:  "$cu"%-4s"$def" of "$nu"%-4s"$def"  "$br"  24hr low: "$lo"%-7s"$def"  "$br"  Maxes: "$hi"%-6s"$def" (10m)  "$lbr"  "$hi"%-6s"$def" (1h)  "$lbr"  "$hi"%-6s"$def" (1d)\n" \
-$cur_m $tot_m $min_m_1d $max_m_10m $max_m_1h $max_m_1d
+printf "%-15s "$br"  "$cu"%-14s"$def" "$br"  "$lo"%-12s"$def" "$br"  "$hi"%-14s"$def" "$lbr"  "$hi"%-14s"$def" "$lbr"  "$hi"%-14s"$def"\n" \
+"CPU Utilisation" $cur_c $min_c_1d $max_c_10m $max_c_1h $max_c_1d
+
+printf "%-15s "$br"  "$cu"%-4s"$def" of "$nu"%-6s"$def" "$br"  "$lo"%-12s"$def" "$br"  "$hi"%-14s"$def" "$lbr"  "$hi"%-14s"$def" "$lbr"  "$hi"%-14s"$def"\n\n" \
+"Memory Usage" $cur_m $tot_m $min_m_1d $max_m_10m $max_m_1h $max_m_1d
